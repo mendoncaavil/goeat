3cf430c2b1e330d0a5bf912d34bd5d7d9a534c59
Avil Mendonca <mendoncaavil@gmail.com>
2022-10-07 19:49:40 +0530

    added hashrouter in the index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import './index.css';
 import App from './App';
 import store from './Store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
 
